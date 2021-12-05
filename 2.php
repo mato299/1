b349a6a1018fb0926a22fb1115b82c2da5902c3a
WickyPayne <v.bednarik@centrum.sk>
2021-12-05 12:40:08 +0100

    viktorove zmeny

--- 2.php
+++ 2.php
@@ -1,10 +1,22 @@
 <?php 
-if(isset($_POST['submit'])){
-    
-    echo "pripojene";
-}
-    
+// 19€ na hraca
+if(isset($_POST['pocet_hracov']))
+    $pocetHracov = $_POST['pocet_hracov'];
 
+// isic -4€ skupinu (z celkovej sumy);
+if(isset($_POST['isic']))
+    $isic = $_POST['isic']; 
 
+// 10% zľava (z celkovej sumy)
+if(isset($_POST['isic']))
+    $isic = $_POST['isic']; 
+
+// - xy €
+if(isset($_POST['suma_darc_poukazu']))
+    $suma = $_POST['suma_darc_poukazu'];
+
+$vysledok = ;
+
+echo($vysledok);
 
 ?>
